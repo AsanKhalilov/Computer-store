2c82e00697d3bf3764e2144bdeb6e89dfecea6ba
AsanKhalilov <khalilov2501@gmail.com>
2022-11-06 15:17:36 +0300

    Page for viewing the product and its characteristics

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,20 +4,33 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {NavLink} from "react-router-dom";
-import { SHOP_ROUTE } from '../utils/consts';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import {Button} from 'react-bootstrap';
 import {observer} from 'mobx-react-lite';
+import { useHistory } from 'react-router-dom';
 
 const NavBar = observer( () => {
     const {user} = useContext(Context)
+    const history = useHistory()
     return (
       <Navbar bg="dark" variant="dark">
         <Container>
           <NavLink style={{color:'white'}} to={SHOP_ROUTE}>MKT</NavLink>
           {user.isAuth ?
             <Nav /*className="ml-auto"*/ style={{color:'white', marginLeft: "auto"}}>
-              <Button variant={"outline-light"}>Админ панель</Button>
-              <Button variant={"outline-light"} /*className="ml-2"*/style={{marginLeft: 8}}>Войти</Button>
+              <Button 
+                variant={"outline-light"} 
+                onClick={() => history.push(ADMIN_ROUTE)}
+              >
+                Админ панель
+              </Button>
+              <Button 
+                variant={"outline-light"} 
+                onClick={() => history.push(LOGIN_ROUTE)}
+                /*className="ml-2"*/style={{marginLeft: 8}}
+              >
+                Выйти
+              </Button>
             </Nav>
             :
             <Nav className="ml-auto" style={{color:'white'}}>
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import { Card, Col, Container, Image, Row , Button} from 'react-bootstrap';
+import RatingStar from "../assets/RatingStar.png"
 
 const DevicePage = () => {
+  const device = {id: 1, name: "SATA 860 Pro", price: 17000, rating: 5, img: `https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png`}
+  const description = [
+    {id:1, title: 'Тип', descriptiion: '2.5" SATA накопитель'},
+    {id:2, title: 'Модель', descriptiion: 'Samsung 860 Pro'},
+    {id:3, title: 'Объем накопителя', descriptiion: '1000 ГБ'},
+    {id:4, title: 'NVMe', descriptiion: 'нет'},
+    {id:5, title: 'Разьем подключения', descriptiion: 'SATA'},
+  ]
+
   return (
-    <div>
-      DevicePage
-    </div>
+    <Container className='mt-3'>
+      <Row>
+        <Col md={6}>
+          <Image width={450} height={450} src={device.img}/>
+        </Col>
+        <Col md={3}>
+          <Row className='d-flex flex-column align-items-center'>
+            <h2>{device.name}</h2>
+            <div
+              className='d-flex align-items-center justify-content-center'     
+              style={{background:`url(${RatingStar}) no-repeat center center`, width:240, height:240, backgroundSize: 'cover', fontSize:64 }}     
+            >
+              {device.rating}
+            </div>
+          </Row>
+        </Col>
+        <Col md={3}>
+          <Card 
+            className='d-flex flex-column align-items-center justify-content-around'
+            style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
+          >
+            <h3>{device.price}</h3>
+            <Button variant={"outline-dark"} >Добавить в корзину</Button>
+          </Card>
+        </Col>
+      </Row>
+      <Row className='d-flex flex-column m-3'>
+        <h2>Характеристики</h2>
+        {description.map((info, index) =>
+          <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+            {info.title}: {info.descriptiion}
+          </Row>
+        )}
+      </Row>
+    </Container>
   );
 };
 
